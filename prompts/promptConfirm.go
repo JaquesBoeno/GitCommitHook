019e656ecda693513877c3683c82e890fc716dc8
JaquesBoeno <jaquesboeno@proton.me>
2023-12-24 16:21:23 -0300

    fix color in (y/N)

--- prompts/promptConfirm.go
+++ prompts/promptConfirm.go
@@ -14,7 +14,7 @@ func confirmUpdate(msg tea.Msg, m Model) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "n", "N":
 			m.confirmResponse = msg.String()
-			m.pastResponses = fmt.Sprint(m.pastResponses, fmt.Sprintf("\033[1m%s\033[0m \033[30m(y/N)\033[0m %s\n", m.currentQuestion.Label, m.confirmResponse))
+			m.pastResponses = fmt.Sprint(m.pastResponses, fmt.Sprintf("\033[1m%s\033[0m (y/N) %s\n", m.currentQuestion.Label, m.confirmResponse))
 
 			for _, question := range m.currentQuestion.Questions {
 				m.Responses = append(m.Responses, commitMessage.Value{
@@ -26,7 +26,7 @@ func confirmUpdate(msg tea.Msg, m Model) (tea.Model, tea.Cmd) {
 			return m.NextQuestion()
 		case "y", "Y":
 			m.confirmResponse = msg.String()
-			m.pastResponses = fmt.Sprint(m.pastResponses, fmt.Sprintf("\033[1m%s\033[0m \033[30m(y/N)\033[0m %s\n", m.currentQuestion.Label, m.confirmResponse))
+			m.pastResponses = fmt.Sprint(m.pastResponses, fmt.Sprintf("\033[1m%s\033[0m (y/N) %s\n", m.currentQuestion.Label, m.confirmResponse))
 			m.Questions = append(m.Questions[:m.index+1], m.Questions[m.index:]...)
 			m.Questions[m.index+1] = m.currentQuestion.Questions[0]
 			return m.NextQuestion()
@@ -38,7 +38,7 @@ func confirmUpdate(msg tea.Msg, m Model) (tea.Model, tea.Cmd) {
 
 func confirmView(m Model) string {
 	str := strings.Builder{}
-	str.WriteString(fmt.Sprintf("%s \033[30m(y/N)\033[0m %s", m.currentQuestion.Label, m.confirmResponse))
+	str.WriteString(fmt.Sprintf("%s (y/N) %s", m.currentQuestion.Label, m.confirmResponse))
 
 	return str.String()
 }
